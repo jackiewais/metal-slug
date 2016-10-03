e47fdf503c353ca6586df10f269a3cc5b21e1b25
Maxi Burastero <maxicc4@gmail.com>
2016-10-03 17:01:08 -0300

    Se agrega una funcion en la clase Direccion del servidor, porque me tiraba error al compilar.

--- src/Servidor/Modelo/Direccion.cpp
+++ src/Servidor/Modelo/Direccion.cpp
@@ -9,4 +9,7 @@ Direccion::~Direccion() {
 	this->direccion;
 }
 
-
+// AGREGO ESTA FUNCION PARA QUE NO ROMPA AL COMPILAR (FALTA IMPLEMENTAR)
+Posicion* Direccion::getSiguientePosicion(Posicion* posicion, int velocidad, int anchoJugador, int anchoEscenario) {
+	return NULL;
+}
