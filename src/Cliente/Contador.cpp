5250650a45c93c0eb847572c3d175335a673fb07
jackiewais <jackiewais@hotmail.com>
2016-11-06 01:16:50 -0300

    Se crea la barra de vida

--- src/Cliente/Contador.cpp
+++ src/Cliente/Contador.cpp
@@ -2,11 +2,12 @@
 
 using namespace std;
 
-Contador::Contador(std::string id, int posX, int posY, Numero* numero) {
+Contador::Contador(std::string id, int posX, int posY, Numero* numero, Progreso* progreso) {
 	this->id = id;
 	this->posX = posX;
 	this->posY = posY;
 	this->numero = numero;
+	this->progreso=progreso;
 }
 
 Contador::~Contador() {
@@ -16,13 +17,19 @@ Contador::~Contador() {
 void Contador::actualizarPuntaje(int dif){
 	puntaje += dif;
 }
+void Contador::actualizarVida(int dif){
+	vida += dif;
+	if (vida==0) vida=100;
+}
 void Contador::renderizar(){
-	int x=posX+100;//pos inicial ultimo digito
+	int x=posX+75;//pos inicial ultimo digito
 	for (int i=1;i<=1000;i=i*10){
 		int digit = puntaje/i%10;
 
 		this->numero->render(digit,x,posY);
 		x-=25;
 	}
+
+	progreso->render(vida,posX,posY+40);
 }
 
