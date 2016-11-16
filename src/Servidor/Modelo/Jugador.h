2ff5f73202a37a29bcc4f310ead37d561dbd2677
Maxi Burastero <maxicc4@gmail.com>
2016-11-15 21:03:45 -0300

    Se arregla un pequenio bug

--- src/Servidor/Modelo/Jugador.h
+++ src/Servidor/Modelo/Jugador.h
@@ -18,7 +18,7 @@ private:
 	int piso;
 	int topeSalto;
 	int plataforma;
-	weapon arma = MACHINEGUN;
+	weapon arma = GUN;
 
 	estadoJugador getEstadoParado();
 	estadoJugador getEstadoSaltarIzquierda();
